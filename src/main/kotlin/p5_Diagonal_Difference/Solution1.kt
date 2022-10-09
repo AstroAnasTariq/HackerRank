b09af3fc8240ad1f6b19518838718df22969661c
Anas Tariq <anasstarek73@gmail.com>
2022-10-10 01:56:16 +0200

    A new problem solved

---
+++ src/main/kotlin/p5_Diagonal_Difference/Solution1.kt
@@ -0,0 +1,56 @@
+package p5_Diagonal_Difference
+
+import java.io.*
+import java.lang.Math.abs
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    var diagonal1 = 0
+    var diagonal2 = 0
+
+    val n = arr.size
+
+    for (i in 0 until n){
+        diagonal1 += arr[i][i]
+        diagonal2 += arr[i][n-1-i]
+    }
+    return kotlin.math.abs(diagonal1 - diagonal2)
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
