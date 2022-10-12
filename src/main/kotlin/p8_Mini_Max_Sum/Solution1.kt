4d9de4af1d52675cdefca4a2f1f77967c638329f
Anas Tariq <anasstarek73@gmail.com>
2022-10-13 01:54:22 +0200

    A new Problem solved

---
+++ src/main/kotlin/p8_Mini_Max_Sum/Solution1.kt
@@ -0,0 +1,46 @@
+package p8_Mini_Max_Sum
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun miniMaxSum(arr: Array<Int>): Unit {
+    // Write your code here
+    var min = Int.MAX_VALUE
+    var max = 0
+    var sum = 0L
+    for (number in arr){
+        sum += number
+        if (number < min) min = number
+        if (number > max) max = number
+    }
+    println("${sum - max} ${sum - min}")
+}
+
+fun main(args: Array<String>) {
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    miniMaxSum(arr)
+}
